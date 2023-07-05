770e421cc8e9b5385cd00725148ad29c0fe28786
nitin23c <nitin.s.chauhan@gmail.com>
2023-07-05 20:55:05 +0530

    terraform formatting

--- docker_dev_server/backend.tf
+++ docker_dev_server/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "awsgeek0-terraform-remote-state"
+    bucket = "demo-terraform-remote-state"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
